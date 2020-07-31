107945341e65dcfb33f620ab4b5f26a68c547a38
Saurabh Burade <sburade3@gmail.com>
2020-07-31 18:59:55 +0530

    deploy setup

--- server/index.js
+++ server/index.js
@@ -21,7 +21,14 @@ app.use(express.json());
 app.use(morgan());
 app.use(cors());
 app.use("/api",userRoutes);
-
+if (process.env.NODE_ENV !== "production") {
+    app.use(express.static(path.join(__dirname, "../client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../client", "build", "index.html")
+        );
+    });
+}
 app.listen(PORT, () => {
     console.log("Connected", PORT);
 });
